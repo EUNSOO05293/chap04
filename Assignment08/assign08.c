d71a642ecb2c83b0fd4f7b62344be9087fed8188
eunsoo <ehfl0489@gmail.com>
2025-04-15 16:11:25 +0900

    push

--- Assignment08/assign08.c
+++ Assignment08/assign08.c
@@ -3,7 +3,7 @@
  내용: 반지름의 길이를 입력받아 구의 부피를 구하는 프로그램을 작성.
  작성자: 이은수
  날짜: 2025.04.11
- 버전 : v0.1
+ 버전 : v0.2
 */
 
 #define _CRT_SECURE_NO_WARNINGS
@@ -11,26 +11,23 @@
 #include <math.h>
 #define PI 3.141592
 
-void SphereVolume(void);
+double SphereVolume(int r);
 
 int main()
 {
-	SphereVolume();
+	int radius;
+	double volume;
+
+	printf("반지름의 길이? ");
+	scanf("%d", &radius);
+
+	volume = SphereVolume(radius);
+	printf("구의 부피: %lf\n", volume);
+
 	return 0;
 }
 
-void SphereVolume(void)
+double SphereVolume(int r)
 {
-	int r;
-	double v;
-	printf("반지름의 길이?");
-	scanf("%d", &r);
-	v = (4.0 / 3.0) * PI * pow(r, 3);
-	printf("구의 부피: %lf", v);
-	return;
+	return (4.0 / 3.0) * PI * pow(r, 3);
 }
-
-
-
-
-
